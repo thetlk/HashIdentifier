fa8bfa7d8abe17e6c413c441ba2ac19553a364b1
tlk <jeremieboutoille@gmail.com>
2013-01-02 20:00:29 +0100

    Initial commit

---
+++ HashIdentifier.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import argparse
+import re
+
+hashes = {"MD5": re.compile(r"^[A-Fa-f0-9]{32}$"),
+          "SHA1": re.compile(r"^[A-Fa-f0-9]{40}$"),
+          "SHA224": re.compile(r"^[A-Fa-f0-9]{56}$"),
+          "SHA256": re.compile(r"^[A-Fa-f0-9]{64}$"),
+          "SHA384": re.compile(r"^[A-Fa-f0-9]{96}$"),
+          "SHA512": re.compile(r"^[A-Fa-f0-9]{128}$")}
+
+def main():
+	
+	parser = argparse.ArgumentParser(description='Identify a hash')
+	parser.add_argument('hash', help='hash to identify')
+	args = parser.parse_args()
+	hashe = args.hash
+	
+	results = []
+	
+	for hashName, hashRegexp in hashes.items():
+	    if hashRegexp.match(hashe):
+	        results.append(hashName)
+	        
+	if len(results) == 0:
+	    print "[-] Unabble to identify the hash"
+	    return
+	
+	if len(results) == 1:
+	    print "[+] Result for '%s' :" % hashe
+	else:
+	    print "[+] '%s' can be :" % hashe
+	    
+	for result in results:
+	    print "\t - %s" % result
+
+if __name__ == '__main__':
+	main()
+
